ae2cb6034c44cc9ed91478d2a27d39a5d4ad224d
Charlie J Gilbride <cjgilbride86@gmail.com>
2021-02-13 21:21:58 +0000

    Update comments
    Improve comments to better describe commands

--- AARGI/CrossFunctions/myRemove_BK.m
+++ AARGI/CrossFunctions/myRemove_BK.m
@@ -21,25 +21,40 @@ Out = reshape(myRemove_in(In2, mm),ss);
         % S single line with length = last dim of In
         % remove the co-relation part of each pixel in In,
         mm = mm(:); 
-%CG: mm = mean of all 28900 pixels per frame.        
+%CG: mm = mean of all 28900 pixels per frame. Similar to y~(i) in Chen et al (2006).        
         nn = numel(mm) - 1;
         mm = mm - mean(mm);
         mm = mm./sqrt(sum(mm.*mm)./nn);
 %CG: now mm represents normalized average for each frame (dividing by
-%std). mm(t) will be larger when more signal occurs in the current frame t.
+%std). mm is the same as the first component dervied from singular value
+%decomposition. We use the mean here instead of svd (as in Chen et al
+%(2006)) because the svd plus regression method is slower when there is
+%only one ROI. In this analysis, we treat the entire field of view as a
+%single ROI for which the background signal is removed. Chen et al (2006)
+%in contrast, calculated the background signal for each ROI individually.
+%Finding the background signal using the regression method would be slower
+%for our data (i.e. single cells expressing GCaMP6s and imaged at high mag
+%to measure spine calcium signals). Using the regression method is also not
+%necessary because the background signal is not as inhomogenous as in Chen
+%et al (2006). Chen et al (2006) did not use a GECI but rather a
+%bath-applied membrane permeable dye to label lots of cells, so their
+%background is higher and more inhomogenous. 
         mx = mean(In, 2);
 %CG: mx = mean of each pixel across time. mx(i) will be larger when the
-%current pixel overlaps with an active part of the cell. 
+%current pixel overlaps with an active part of the cell. mx is an offset
+%that is removed from the pixel signal. 
         ddx = repmat(mx,1,nn + 1);
         X = In - ddx;
 %CG: X contains the background of In 
         c = X * mm./nn;
 %CG: c is the pixelwise cross-correlation coefficient between the input
 %data with mean 0 (X) and the total average with mean 0 (mm). 
-%CG: c(i) will be larger when background is higher (why divide by nn?). c
-%is the correlated part of each pixel of In
+%CG: c(i) will be larger when background is higher. The product of c*mm'
+%will extract the noise component from the signal which can then be
+%subtracted from the data matrix. 
         Out = In - c * mm';   
-%CG: why multiply by mm again? 
 
     end
 end
+
+%Reference: Chen et al (2006). PMID:16387783
